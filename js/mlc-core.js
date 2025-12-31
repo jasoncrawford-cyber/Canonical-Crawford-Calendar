7cd7df94621dc67c310ba0d1b737806d65dae405
jasoncrawford-cyber <jason.crawford@dreams2reality.homes>
2025-12-31 11:45:31 -0600

    Create mlc-core.js

---
+++ js/mlc-core.js
@@ -0,0 +1,45 @@
+const CYCLE_YEARS = 334;
+const TOTAL_MONTHS = 4131;
+
+function isLeapMonthYear(y) {
+  return (
+    Math.floor((TOTAL_MONTHS * y) / CYCLE_YEARS) -
+      Math.floor((TOTAL_MONTHS * (y - 1)) / CYCLE_YEARS) ===
+    13
+  );
+}
+
+function generateMonths(year) {
+  const months = [];
+  let lunar = 0;
+  const leap = isLeapMonthYear(year);
+
+  for (let i = 1; i <= 12; i++) {
+    lunar++;
+    months.push({
+      name: `Month ${i}`,
+      length: lunar % 2 ? 30 : 29,
+      leap: false,
+    });
+
+    if (leap && i === 6) {
+      months.push({
+        name: `Month 6 (Leap)`,
+        length: lunar % 2 ? 30 : 29,
+        leap: true,
+      });
+    }
+  }
+
+  return months;
+}
+
+const WEEKDAYS = [
+  "Foreday",
+  "Neistday",
+  "Midday",
+  "Gangday",
+  "Fendday",
+  "Restday",
+  "Yondday",
+];
