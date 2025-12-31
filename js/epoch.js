696b263f095a1942af3ea72362d543d6cdf97534
jasoncrawford-cyber <jason.crawford@dreams2reality.homes>
2025-12-31 16:21:25 -0600

    Update epoch.js

--- js/epoch.js
+++ js/epoch.js
@@ -1,3 +1,9 @@
-// Epoch definition:
-// MLC Year 1, Eastren 1 = Gregorian 2000-03-20 (March Equinox)
+// ======================================================
+// Epoch definition (single source of truth)
+//
+// MLC Year 1, Eastren 1
+// = Gregorian 2000-03-20 (March Equinox)
+// ======================================================
+
+// NOTE: gregorianToJDN MUST already be loaded
 const EPOCH_JDN = gregorianToJDN(2000, 3, 20);
